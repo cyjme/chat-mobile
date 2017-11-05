4d866c527a98c90c69a77dc58171e94eb25eea7e
Jason <changyuanjian@gmail.com>
2017-11-05 14:08:59 +0800

    init

---
+++ src/router.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Chat from '@/route/chat';
+import ContactList from '@/route/contactList';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/user/:accToken',
+      name: 'ContactList',
+      component: ContactList,
+    },
+    {
+      path: '/chat',
+      name: 'Chat',
+      component: Chat,
+    },
+  ],
+});
