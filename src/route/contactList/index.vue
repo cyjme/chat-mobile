834a463a0329f4a57dc25e85fe43edbae2d789c0
Jason <changyuanjian@gmail.com>
2017-12-27 16:31:06 +0800

    mod: add last-read-at

--- src/route/contactList/index.vue
+++ src/route/contactList/index.vue
@@ -1,7 +1,7 @@
 <template>
-<div id="chatbox">
-	<div id="friendslist">
-    	<div id="topmenu">
+  <div id="chatbox">
+    <div id="friendslist">
+      <div id="topmenu">
         <div class="left"></div>
         <div class="right">
           <span>
@@ -11,44 +11,51 @@
       </div>
 
       <div id="friends">
-      	<div class="friend" v-for="contact in contacts" :key="contact.token" @click="handleClick(contact.userId)">
-            <div v-if="contact.avt==''?true:false" class="text-avatar">{{contact.nick.substring(0,1)}}</div>
-            <img v-if="contact.avt==''?false:true" :src="contact.avt" />
-              <p>
-              	<strong>{{contact.nick}}</strong>
-                  <br />
-	              <span>{{contact.phone}}</span>
-              </p>
-              <div class="status available"></div>
-          </div>
+        <div class="friend" v-for="contact in contacts" :key="contact.token" @click="handleClick(contact.userId)">
+          <div v-if="contact.avt==''?true:false" class="text-avatar">{{contact.nick.substring(0,1)}}</div>
+          <img v-if="contact.avt==''?false:true" :src="contact.avt"/>
+          <p>
+            <strong>{{contact.nick}}</strong>
+            <br/>
+            <span>{{contact.phone}}</span>
+          </p>
+          <div class="status available"></div>
+        </div>
 
-          <div id="search">
-	          <input type="text" id="searchfield" placeholder="Search contacts..." />
-          </div>
-      </div>                
-    </div>	
-</div>	
+        <div id="search">
+          <input type="text" id="searchfield" placeholder="Search contacts..."/>
+        </div>
+      </div>
+    </div>
+  </div>
 
 </template>
 
 <script>
-import { scrollBottom } from "../../help";
-export default {
-  name: "contactList",
-  props: ["contacts", "newMsg", "currentUser"],
-  created: function() {},
-  methods: {
-    handleClick(toUserId) {
-      this.$router.push({
-        name: "Chat",
-        params: {
-          toUser: toUserId 
-        }
-      });
-      scrollBottom();
+  import {scrollBottom} from "../../help";
+
+  export default {
+    name: "contactList",
+    props: ["contacts", "newMsg", "currentUser"],
+    created: function () {
+    },
+    methods: {
+      updateLastReadAt(toUserId) {
+        this.axios.get(`/update-last-read-at/${toUserId}`).then(() => {
+        });
+      },
+      handleClick(toUserId) {
+        this.updateLastReadAt(toUserId);
+        this.$router.push({
+          name: "Chat",
+          params: {
+            toUser: toUserId
+          }
+        });
+        scrollBottom();
+      }
     }
-  }
-};
+  };
 </script>
 
 <style>
